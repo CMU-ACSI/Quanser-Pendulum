ed8601fe39b179819fd4064355a08e488755bd75
ExhAustin <aswang@andrew.cmu.edu>
2018-11-27 20:29:17 -0500

    Cleaned up controller

--- embedded/controller.ino
+++ embedded/controller.ino
@@ -23,7 +23,37 @@ www.cmu.edu
 #include "QUBEServo.h"	// QUBE Servo library
 #include "ACSI_lib.h"
 
-// Don't touch the setup unless you decide to use interrupts
+// DECLARE GLOBAL VARIABLES HERE IF NEEDED
+
+// Discrete-time controller (MODIFY THIS FUNCTION)
+void controller_step() {
+	// Accessible global variables:
+	// 		Input variables:
+	//				seconds - (float) Time elapsed since start of program in seconds
+	//				theta - (float) Pendulum angle radians
+	//				alpha - (float) Arm angle in radians
+	// 		Output variables:
+	//				motorVoltage - (float) Signal sent to the motor in Volts
+	//				LEDRed - (float) Red LED intensity on a scale of 0 to 999
+	//				LEDGreen - (float) Green LED intensity on a scale of 0 to 999
+	//				LEDBlue - (float) Blue LED intensity on a scale of 0 to 999
+
+	// Below demonstrates changing the LED state (you probably don't care) 
+	// and changing the motor voltage (you certainly DO care)
+	if (theta <= -(20*M_PI/180.0)) {
+		LEDRed = 999;
+		LEDGreen = 0;
+		LEDBlue = 0;
+		motorVoltage = -1.0;
+	} else if (theta >= (20*M_PI/180.0)) {
+		LEDRed = 0;
+		LEDGreen = 0;
+		LEDBlue = 999;
+		motorVoltage = 1.0;
+	}
+}
+
+// This function will be called once during initialization
 void setup() {
   // Set the slaveSelectPin as an output
   pinMode(slaveSelectPin, OUTPUT);
@@ -35,10 +65,17 @@ void setup() {
   // (Note that 250000 baud must be selected from the drop-down list on the Arduino
   // Serial Monitor for the data to be displayed properly.)
   Serial.begin(250000);
+
+	// Initialize global variables (CHANGE HYPERPARAMETERS HERE)
+	sampleTime = 0.002;
+	sampleMicros = (unsigned long)sampleTime*1000000;
+
+	// ADDITIONAL INITIALIZATIONS HERE IF NEEDED (ex: GENERATE REFERENCE OFFLINE)
+
 }
 
+// This function will be called repeatedly until Arduino is reset
 void loop() {
-  
   // Reset after the Arduino power is cycled or the reset pushbutton is pressed
   if (startup) {
     resetQUBEServo();
@@ -48,43 +85,34 @@ void loop() {
   // If the difference between the current time and the last time an SPI transaction
   // occurred is greater than the sample time, start a new SPI transaction
   // (alternatively, use a timer interrupt)
-  unsigned long currentMicros = micros();
-  if (currentMicros - previousMicros >= sampleTime) {
-    previousMicros = previousMicros + sampleTime;
+  currentMicros = micros();
+  if (currentMicros - previousMicros >= sampleTime*1000000) {
+    previousMicros = previousMicros + sampleTime*1000000;
 
-		// Read data into global variables. For variable definitions, see ACSI_lib.h.
+		// Read data into global variables. For variable definitions, see above comments.
 		readSensors();
+
 		// This will define the data that will be displayed at the serial terminal.
 		displayData.buildString(theta, alpha, currentSense, moduleID, moduleStatus);
 
-		// Below demonstrates changing the LED state (you probably don't care) and changing the motor voltage (you certainly DO care)
-		if (theta <= -(20*M_PI/180.0)) {
-			LEDRed = 999;
-			LEDGreen = 0;
-			LEDBlue = 0;
-			motorVoltage = -1.0;
-		} else if (theta >= (20*M_PI/180.0)) {
-			LEDRed = 0;
-			LEDGreen = 0;
-			LEDBlue = 999;
-			motorVoltage = 1.0;
-		}
+		// Run discrete controller timestep
+		controller_step();
+
 		// Write the data to the Qube servo
 		driveMotor();
   }
 
-  
-  // print data to the Arduino Serial Monitor in between SPI transactions
+  // Print data to the Arduino Serial Monitor in between SPI transactions
   // (Note that the Serial.print() function is time consuming.  Printing the entire
   // string at once would exceed the sample time required to balance the pendulum.)
   else {  //We're in between samples
-    // only print if there's a string ready to be printed, and there's enough time before the next SPI transaction
+    // Only print if there's a string ready to be printed, and there's enough time before the next SPI transaction
     if ( (displayData.dDataReady) && (currentMicros - previousMicros <= (sampleTime - 100)) ) {
-      // if there is room available in the serial buffer, print one character
+      // If there is room available in the serial buffer, print one character
       if(Serial.availableForWrite() > 0) {
         Serial.print(displayData.dData[displayData.dDataIndex]);
         displayData.dDataIndex = displayData.dDataIndex + 1;
-        // if the entire string has been printed, clear the flag so a new string can be obtained
+        // If the entire string has been printed, clear the flag so a new string can be obtained
         if(displayData.dDataIndex == displayData.dData.length()) {
           displayData.dDataReady = false;
         }
@@ -92,4 +120,3 @@ void loop() {
     }
   }
 }
-
